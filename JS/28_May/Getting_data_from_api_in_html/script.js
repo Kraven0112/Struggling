aed3bcf17b4710fd524abbe7a11d0005e0c784cf
Kraven0112 <167796699+Kraven0112@users.noreply.github.com>
2024-05-28 15:13:28 +0545

    add

---
+++ JS/28_May/Getting_data_from_api_in_html/script.js
@@ -0,0 +1,24 @@
+const input=document.getElementById('city-input')
+const button =document.getElementById('search-button')
+const cityName=document.getElementById('city-name')
+const cityTime = document.getElementById('city-time')
+const cityTemp = document.getElementById('city-temp')
+
+async function getApi(cityName)
+{
+    const data = await fetch(`https://api.weatherapi.com/v1/current.json?key=5a15dadcce3648c8a3d84048242805&q=${cityName}&aqi=no`)
+
+     return await data.json() //waiting for data 
+   
+}
+
+button.addEventListener('click',async()=>{
+    const value= input.value  
+    const actualData = await getApi(value)  //getApi will take value from here and make search in api above in async function
+
+    cityName.innerText = `Location : ${actualData.location.name},${actualData.location.region},${actualData.location.country}`
+
+    cityTime.innerText = `Date and Time : ${actualData.current.last_updated}`
+
+    cityTemp.innerText = `Temperature in celsius : ${actualData.current.temp_c}`
+})
